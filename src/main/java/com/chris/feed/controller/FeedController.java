6d88df76329881cdfe524a55cfa5f0c3e397e253
Christian Magro <christian.magro@melitaltd.com>
2020-01-30 10:45:19 +0100

    Update API url

--- src/main/java/com/chris/feed/controller/FeedController.java
+++ src/main/java/com/chris/feed/controller/FeedController.java
@@ -6,6 +6,7 @@ import com.chris.feed.dto.TypeStatus;
 import com.chris.feed.service.SportFeedsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class FeedController {
 
     private final SportFeedsService sportFeedsService;
 
-    @GetMapping("/sportfeed")
+    @GetMapping("/livescore")
     public List<Sport> getFeeds(@RequestParam(required = false) final List<TypeStatus> statuses, @RequestParam(required = false) final LiveStatus liveStatus) {
 
         return sportFeedsService.getSportFeeds(statuses, liveStatus);
